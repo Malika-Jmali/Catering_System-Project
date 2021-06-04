3ceae020d48f71ab76b3b7cfa3f0c0b825e580be
malika jmali <jmalimalika6@gmail.com>
2021-06-04 13:01:03 -0400

    Malika2

--- m1-capstone-java-cateringsystem/src/main/java/com/techelevator/view/CashRegister.java
+++ m1-capstone-java-cateringsystem/src/main/java/com/techelevator/view/CashRegister.java
@@ -2,12 +2,30 @@ package com.techelevator.view;
 
 public class CashRegister {
 
-//    //instance variable
-//    private int balance;
-//
-//
-//    public int addMoney
+//instance variable
+   private double balance;
 
+   public CashRegister(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public double addMoney(double dollar){
+        balance= balance + dollar;
+        return balance;
+    }
+    public double giveChange(double totalCost){
+       balance = balance - totalCost;
+       return balance;
+    }
+// using the smallest amount of bills and coins possible
+    // The customer’s current balance should be updated to \$0 remaining.
 
 
 
---
+++ m1-capstone-java-cateringsystem/src/main/java/com/techelevator/view/ShoppingCart.java
@@ -0,0 +1,28 @@
+package com.techelevator.view;
+
+import com.techelevator.Product;
+
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Product> productsSelected;
+
+    public void addItems(Product items){
+    productsSelected.add(items);
+    }
+
+    public void emptyCart(){
+     productsSelected.clear();
+    }
+
+
+    public List<Product> getShoppingCartItem(){
+        return productsSelected;
+
+    }
+// empty card return null
+// public List getShoppingCartItem (quantity, type, product, itemCost)
+    // Transaction
+    /*If the product code does not exist, the customer is informed that the product code they selected does not exist and the customer is returned to the Purchase menu (sub-menu).
+If a product is sold out, the customer is informed and returned to the Purchase menu (sub menu).*/
+}
